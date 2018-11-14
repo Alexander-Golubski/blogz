205b5ac243244d13237e05a13018d1d87af8f019
Alexander-Golubski <algolubski@gmail.com>
2018-11-14 13:31:19 -0600

    added login and register templates, user class

--- main.py
+++ main.py
@@ -3,12 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildthisblog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:cheesy@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 app.secret_key = 'y337kGcys&zP3B'
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship("Blog", backref="owner") 
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 class Blog(db.Model):
 #inherits some traits from existing class Model within SQLAlchemy
@@ -16,11 +27,12 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
-    #owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
@@ -39,6 +51,8 @@ def newpost():
     title_error = ''
     body_error = ''
 
+    owner = User.query.filter_by(username=session['username']).first()
+
     if request.method == 'POST':
         
         blog_title = request.form['title']
@@ -50,7 +64,8 @@ def newpost():
             body_error = 'Please enter the body text.'
 
         if not title_error and not body_error:
-            new_post = Blog(blog_title, body)
+            
+            new_post = Blog(blog_title, body, owner)
             db.session.add(new_post)
             db.session.commit()
 
